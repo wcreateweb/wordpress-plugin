bf19937d55e6a99e5ddaefdf13363d7748b66cb9
tijmen <tijmenbruggeman@gmail.com>
2025-09-02 12:37:02 +0200

    feat: add backup class

---
+++ src/class-tiny-backup.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* Tiny Compress Images - WordPress plugin.
+* Copyright (C) 2015-2018 Tinify B.V.
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation; either version 2 of the License, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc., 51
+* Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+/*
+ * Tiny_Backup provides functionality for backing up images before compression.
+ * When enabled, back-up will add hooks to the `tinify_before_compress` and 
+ * the `tinify_before_bulk_compress` filters.
+ * 
+ */
+class Tiny_Backup {
+    
+    // add option for updraft plus
+    // add option for local backup
+}
