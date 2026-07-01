2edd5760cf283bc031d5213fa8e144c13bedd6ac
tijmen <tijmenbruggeman@gmail.com>
2026-07-01 09:28:48 +0200

    move bootstrap to bootstrap file

---
+++ test/bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once dirname( __FILE__ ) . '/../vendor/autoload.php';
+require_once dirname( __FILE__ ) . '/unit/Tiny_TestCase_V2.php';
+require_once dirname( __FILE__ ) . '/helpers/wordpress-cli.php';
+require_once dirname( __FILE__ ) . '/../src/config/class-tiny-config.php';
+require_once dirname( __FILE__ ) . '/../src/compatibility/wpml/class-tiny-wpml.php';
+require_once dirname( __FILE__ ) . '/../src/compatibility/as3cf/class-tiny-as3cf.php';
+require_once dirname( __FILE__ ) . '/../src/compatibility/woocommerce/class-tiny-woocommerce.php';
+
+function plugin_autoloader( $class ) {
+	$file = dirname( __FILE__ ) . '/../src/class-' .
+		str_replace( '_', '-', strtolower( $class ) ) . '.php';
+
+	if ( file_exists( $file ) ) {
+		include $file;
+	}
+}
+
+spl_autoload_register( 'plugin_autoloader' );
+
+class Tiny_PHP {
+	public static $fopen_available = true;
+	public static $client_supported = true;
+
+	public static function fopen_available() {
+		return self::$fopen_available;
+	}
+
+	public static function client_supported() {
+		return self::$client_supported;
+	}
+}
--- test/unit/TinyTestCase.php
+++ test/unit/TinyTestCase.php
@@ -13,32 +13,6 @@ require_once 'vendor/autoload.php';
 use org\bovigo\vfs\vfsStream;
 use \PHPUnit\Framework\TestCase;
 
-function plugin_autoloader( $class ) {
-	$file = dirname( __FILE__ ) . '/../../src/class-' .
-		str_replace( '_', '-', strtolower( $class ) ) . '.php';
-
-	if ( file_exists( $file ) ) {
-		include $file;
-	} else {
-		spl_autoload( $class );
-	}
-}
-
-spl_autoload_register( 'plugin_autoloader' );
-
-class Tiny_PHP {
-	public static $fopen_available = true;
-	public static $client_supported = true;
-
-	public static function fopen_available() {
-		return self::$fopen_available;
-	}
-
-	public static function client_supported() {
-		return self::$client_supported;
-	}
-}
-
 abstract class Tiny_TestCase extends TestCase {
 	protected $wp;
 	protected $vfs;
